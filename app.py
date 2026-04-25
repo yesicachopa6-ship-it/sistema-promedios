ffa51c2806f742d0685401f205519bc188fc8bd4
yesicachopa6-ship-it <yesicachopa6@gmail.com>
2026-04-24 23:58:58 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-import os from flask import Flask, render_template, request, redirect, url_for
+import os
+from flask import Flask, render_template, request, redirect, url_for
 import psycopg2
 
 app = Flask(__name__)
